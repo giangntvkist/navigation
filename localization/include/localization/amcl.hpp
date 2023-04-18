274687b9a8d6b58e5d1c43a647555c6fc306216c
giangntvkist <giangnt.vkist@gmail.com>
2023-04-18 23:32:21 +0700

    Code AMCL

---
+++ localization/include/localization/amcl.hpp
@@ -0,0 +1,44 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
+
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/MapMetaData.h>
+
+#include <geometry_msgs/PoseArray.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf/transform_broadcaster.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <random>
+#include <fstream>
+#include <unistd.h>
+
+#include "localization/mylib.hpp"
+#include "localization/motion_model.hpp"
+#include "localization/observation_model.hpp"
+
+class AMCL {
+    public:
+        void init();
+        void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& map);
+        void dataCallback(const sensor_msgs::LaserScan::ConstPtr& scan, const nav_msgs::Odometry& odom);
+    private:
+        
+}; 
